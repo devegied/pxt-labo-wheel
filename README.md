4432aa4d46b3592d916a2c423e8163a4beed580c
devegied <24810903+devegied@users.noreply.github.com>
2020-11-01 00:31:37 +0200

    version 0.0.1

---
+++ README.md
@@ -0,0 +1,13 @@
+# Micro:Bit on Nintendo Labo wheel
+
+A set of functions for using Micro:bit attached to Nintendo Labo wheel as a remote controller
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+
+(The metadata above is needed for package search.)
---
+++ labowheel.cpp
@@ -0,0 +1,16 @@
+#include "pxt.h"
+
+using namespace pxt;
+
+namespace labowheel {
+  // Get wheel rotation angle
+  //%
+  int getYaw(){
+    Sample3D sample = uBit.accelerometer.getSample();
+    double x = (double) sample.x;
+    double y = (double) sample.y;
+    double z = (double) sample.z;
+    double yaw = atan2(x, -y);
+    return (int) ((360*yaw) / (2*PI));
+  }
+}
